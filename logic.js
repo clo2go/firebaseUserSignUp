d9f8c105c269af65bce65ff9d13af405f528b034
clo2go <clo2go@gmail.com>
2017-01-25 11:30:36 -0600

    added cards and alert to boostrap, started firebase intialization

---
+++ logic.js
@@ -0,0 +1,10 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB4cagvjI7_ZjY7S9GIcqgOFElMGQv8QL8",
+    authDomain: "user-sign-up-ed08b.firebaseapp.com",
+    databaseURL: "https://user-sign-up-ed08b.firebaseio.com",
+    storageBucket: "user-sign-up-ed08b.appspot.com",
+    messagingSenderId: "214886211251"
+  };
+  firebase.initializeApp(config);
